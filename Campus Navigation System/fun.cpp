b639fe3723af0d3eeca832deeb7b3151a46264ef
Smlbz <Smlbz@outlook.com>
2024-03-06 10:30:26 +0800

    #

---
+++ Campus Navigation System/fun.cpp
@@ -0,0 +1,4 @@
+#include"fun.h"
+int add(int a, int b) {
+	return a + b;
+}
---
+++ Campus Navigation System/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"fun.h"
+using namespace std;
+int main() {
+	cout << "hehe" << endl;
+	cout << add(3, 5) << endl;
+	return 0;
+}
